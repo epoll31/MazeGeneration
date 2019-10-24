40978a494977749bc8cfbc4af29ea9c77a73152b
Ethan Pollack <Ethan.Pollack@GMR.local>
2019-10-23 17:32:57 -0700

    Added QuickFind UnionFind Method

---
+++ MazeGeneration/MazeGeneration/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace MazeGeneration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("Data.txt");
+            QuickFind uf = new QuickFind(int.Parse(lines[0]));
+            for (int i = 1; i < lines.Length; i++)
+            {
+                uf.Union(int.Parse(lines[i].Split(" ")[0]), int.Parse(lines[i].Split(" ")[1]));
+            }
+            ;
+        }
+    }
+}
---
+++ MazeGeneration/MazeGeneration/QuickFind.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MazeGeneration
+{
+    public class QuickFind
+    {
+        private int[] Data;
+
+        public QuickFind(int number)
+        {
+            Data = new int[number];
+
+            for (int i = 0; i < number; i++)
+            {
+                Data[i] = i;
+            }
+        }
+
+        public void Union(int p, int q)
+        {
+            if (AreConnected(p, q))
+            {
+                return;
+            }
+
+            int pValue = Find(p);
+            int qValue = Find(q);
+            for (int i = 0; i < Data.Length; i++)
+            {
+                if (Find(i) == pValue)
+                {
+                    Data[i] = qValue;
+                }
+            }
+        }
+
+        public int Find(int p)
+        {
+            return Data[p];
+        }
+
+        public bool AreConnected(int p, int q)
+        {
+            return Find(p) == Find(q);
+        }
+    }
+}
