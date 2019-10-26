cac5ff570955781d5574cc764a3224d39aa76d7d
Ethan Pollack <epollack31@gmail.com>
2019-10-26 16:23:48 -0700

    Added Weighted Quick Union

---
+++ MazeGeneration/MazeGeneration/WeightedQuickUnion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MazeGeneration
+{
+    public class WeightedQuickUnion : QuickUnion
+    {
+        private Dictionary<int, int> Lengths;
+
+        public WeightedQuickUnion(int number)
+            : base (number)
+        {
+            Lengths = new Dictionary<int, int>(number);
+            for (int i = 0; i < number; i++)
+            {
+                Lengths.Add(i, 1);
+            }
+        }
+
+        public override void Union(int p, int q)
+        {
+            if (AreConnected(p, q))
+            {
+                return;
+            }
+
+            if (Lengths[p] < Lengths[q])
+            {
+                Data[p] = q;
+            }
+            else
+            {
+                Data[q] = p;
+            }
+        }
+    }
+}
